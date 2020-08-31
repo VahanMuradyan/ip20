fa82d466a4526a57f49627c0d3564145ad301443
VahanMuradyan <10vahan.m16@gmail.com>
2020-08-31 18:49:07 +0400

    Event Information Section

---
+++ static/ip20.01e945506d7bcbf70f53.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("ip20",{
+
+/***/ "./packages/twentytwenty-theme/src/components/home.js":
+/*!************************************************************!*\
+  !*** ./packages/twentytwenty-theme/src/components/home.js ***!
+  \************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _EventInformation__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./EventInformation */ \"./packages/twentytwenty-theme/src/components/EventInformation.js\");\n/* harmony import */ var _Countdown__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Countdown */ \"./packages/twentytwenty-theme/src/components/Countdown.js\");\n/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @emotion/core */ \"./node_modules/@emotion/core/dist/core.browser.esm.js\");\nvar Home=()=>{return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[\"jsx\"])(\"div\",{className:\"homepage-container\"},Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[\"jsx\"])(_EventInformation__WEBPACK_IMPORTED_MODULE_1__[\"default\"],null));};/* harmony default export */ __webpack_exports__[\"default\"] = (Home);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWNrYWdlcy90d2VudHl0d2VudHktdGhlbWUvc3JjL2NvbXBvbmVudHMvaG9tZS5qcz84OGQwIl0sIm5hbWVzIjpbIkhvbWUiXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUlBLEdBQU1BLEtBQUksQ0FBRyxJQUFNLENBQ2pCLE1BQ0Usa0VBQUssU0FBUyxDQUFDLG9CQUFmLEVBQ0UsMERBQUMseURBQUQsTUFERixDQURGLENBTUQsQ0FQRCxDQVNlQSxtRUFBZiIsImZpbGUiOiIuL3BhY2thZ2VzL3R3ZW50eXR3ZW50eS10aGVtZS9zcmMvY29tcG9uZW50cy9ob21lLmpzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gXCJyZWFjdFwiO1xyXG5pbXBvcnQgRXZlbnRJbmZvcm1hdGlvbiBmcm9tIFwiLi9FdmVudEluZm9ybWF0aW9uXCI7XHJcbmltcG9ydCBDb3VudGRvd24gZnJvbSBcIi4vQ291bnRkb3duXCI7XHJcblxyXG5jb25zdCBIb21lID0gKCkgPT4ge1xyXG4gIHJldHVybiAoXHJcbiAgICA8ZGl2IGNsYXNzTmFtZT1cImhvbWVwYWdlLWNvbnRhaW5lclwiPlxyXG4gICAgICA8RXZlbnRJbmZvcm1hdGlvbiAvPlxyXG4gICAgICB7LyogPENvdW50ZG93biAvPiAqL31cclxuICAgIDwvZGl2PlxyXG4gICk7XHJcbn07XHJcblxyXG5leHBvcnQgZGVmYXVsdCBIb21lO1xyXG4iXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./packages/twentytwenty-theme/src/components/home.js\n");
+
+/***/ })
+
+})
---
+++ static/ip20.ffbff3f3d3175e44b529.hot-update.js
@@ -0,0 +1,27 @@
+webpackHotUpdate("ip20",{
+
+/***/ "./packages/twentytwenty-theme/src/components/Countdown.js":
+/*!*****************************************************************!*\
+  !*** ./packages/twentytwenty-theme/src/components/Countdown.js ***!
+  \*****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @emotion/core */ \"./node_modules/@emotion/core/dist/core.browser.esm.js\");\nvar Countdown=()=>{return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_1__[\"jsx\"])(\"div\",null,Object(_emotion_core__WEBPACK_IMPORTED_MODULE_1__[\"jsx\"])(\"h1\",null,\"Countdown\"));};/* harmony default export */ __webpack_exports__[\"default\"] = (Countdown);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWNrYWdlcy90d2VudHl0d2VudHktdGhlbWUvc3JjL2NvbXBvbmVudHMvQ291bnRkb3duLmpzPzJiOTciXSwibmFtZXMiOlsiQ291bnRkb3duIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFBQTtBQUVBLEdBQU1BLFVBQVMsQ0FBRyxJQUFNLENBQ3RCLE1BQ0Usc0VBQ0UsZ0ZBREYsQ0FERixDQUtELENBTkQsQ0FRZUEsd0VBQWYiLCJmaWxlIjoiLi9wYWNrYWdlcy90d2VudHl0d2VudHktdGhlbWUvc3JjL2NvbXBvbmVudHMvQ291bnRkb3duLmpzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcclxuXHJcbmNvbnN0IENvdW50ZG93biA9ICgpID0+IHtcclxuICByZXR1cm4gKFxyXG4gICAgPGRpdj5cclxuICAgICAgPGgxPkNvdW50ZG93bjwvaDE+XHJcbiAgICA8L2Rpdj5cclxuICApXHJcbn1cclxuXHJcbmV4cG9ydCBkZWZhdWx0IENvdW50ZG93bjsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./packages/twentytwenty-theme/src/components/Countdown.js\n");
+
+/***/ }),
+
+/***/ "./packages/twentytwenty-theme/src/components/home.js":
+/*!************************************************************!*\
+  !*** ./packages/twentytwenty-theme/src/components/home.js ***!
+  \************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _EventInformation__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./EventInformation */ \"./packages/twentytwenty-theme/src/components/EventInformation.js\");\n/* harmony import */ var _Countdown__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Countdown */ \"./packages/twentytwenty-theme/src/components/Countdown.js\");\n/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @emotion/core */ \"./node_modules/@emotion/core/dist/core.browser.esm.js\");\nvar Home=()=>{return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[\"jsx\"])(\"div\",{className:\"homepage-container\"},Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[\"jsx\"])(_EventInformation__WEBPACK_IMPORTED_MODULE_1__[\"default\"],null),Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[\"jsx\"])(_Countdown__WEBPACK_IMPORTED_MODULE_2__[\"default\"],null));};/* harmony default export */ __webpack_exports__[\"default\"] = (Home);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWNrYWdlcy90d2VudHl0d2VudHktdGhlbWUvc3JjL2NvbXBvbmVudHMvaG9tZS5qcz84OGQwIl0sIm5hbWVzIjpbIkhvbWUiXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUlBLEdBQU1BLEtBQUksQ0FBRyxJQUFNLENBQ2pCLE1BQ0Usa0VBQUssU0FBUyxDQUFDLG9CQUFmLEVBQ0UsMERBQUMseURBQUQsTUFERixDQUVFLDBEQUFDLGtEQUFELE1BRkYsQ0FERixDQU1ELENBUEQsQ0FTZUEsbUVBQWYiLCJmaWxlIjoiLi9wYWNrYWdlcy90d2VudHl0d2VudHktdGhlbWUvc3JjL2NvbXBvbmVudHMvaG9tZS5qcy5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tIFwicmVhY3RcIjtcclxuaW1wb3J0IEV2ZW50SW5mb3JtYXRpb24gZnJvbSBcIi4vRXZlbnRJbmZvcm1hdGlvblwiO1xyXG5pbXBvcnQgQ291bnRkb3duIGZyb20gXCIuL0NvdW50ZG93blwiO1xyXG5cclxuY29uc3QgSG9tZSA9ICgpID0+IHtcclxuICByZXR1cm4gKFxyXG4gICAgPGRpdiBjbGFzc05hbWU9XCJob21lcGFnZS1jb250YWluZXJcIj5cclxuICAgICAgPEV2ZW50SW5mb3JtYXRpb24gLz5cclxuICAgICAgPENvdW50ZG93biAvPlxyXG4gICAgPC9kaXY+XHJcbiAgKTtcclxufTtcclxuXHJcbmV4cG9ydCBkZWZhdWx0IEhvbWU7XHJcbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./packages/twentytwenty-theme/src/components/home.js\n");
+
+/***/ })
+
+})
